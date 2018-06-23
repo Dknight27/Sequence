787ca8a3b4e5922ed7ce5eccdf6153b6eeb5bc22
Dknight27 <978864703@qq.com>
2018-06-23 11:00:16 +0800

    Happy!

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include"Sequence.h"
+#include<fstream>
+#include<string>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    Sequence test("dna.txt");
+    cout<<test.length()<<endl;
+    cout<<numberOf()<<end;
+    cout<<test.longestConsecutive()<<endl;
+    cout<<test.longestRepeated();
+}
